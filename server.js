f7cc37a5c9b9d165eef889b402a7e31cfdf8028a
Jeyakumar S <132358866+Jeyakumar-05@users.noreply.github.com>
2025-06-20 07:00:42 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const dbConn = require('./config/db'); // Ensure your database connection is initialized
+const Pets = require('./routes/petRoutes');
+const Contact = require('./routes/contactRoute');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ 
+
+    // origin: 'http://localhost:5173',
+    origin:'https://pet-adoption-client-theta.vercel.app',
+
+    methods: ['GET', 'POST' ,'DELETE'],
+     credentials: true,
+
+    
+ })); 
+
+const port = process.env.PORT || 7778;
+
+app.use('/contact', Contact);
+app.use('/pets', Pets);
+
+app.get('/', (req, res) => {
+    res.status(200).json("Welcome"); 
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
